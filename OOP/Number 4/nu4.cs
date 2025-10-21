e77595117d5664779f78385e53dcd8ba52ea467a
HoshinoRe <ggamsenitro@gmail.com>
2025-10-22 00:43:13 +0300

    Add files via upload

---
+++ OOP/Number 4/nu4.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp23;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(10);
+            circle.GetArea();
+        }
+    }
+}
