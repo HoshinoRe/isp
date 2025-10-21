cc20be20651fdc9b8932cf656459bc4f43596212
HoshinoRe <ggamsenitro@gmail.com>
2025-10-22 00:20:12 +0300

    Add files via upload

---
+++ OOP/Numbet 3/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  ConsoleApp22
+{
+    class BankAccount
+    {
+        private string Owner;
+        private double Balance;
+
+        public BankAccount(string accOwner, double accBalance)
+        {
+            Owner = accOwner;
+
+            if (accBalance >= 0)
+            {
+                Balance = accBalance;
+            }
+            else
+            {
+                Balance = 0;
+                Console.WriteLine("Баланс не может быть отрицательным!");
+            }
+        }
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                Balance += amount;
+                Console.WriteLine($"{Owner}, баланс: {Balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Сумма должна быть больше 0!");
+            }
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= Balance)
+            {
+                Balance -= amount;
+                Console.WriteLine($"{Owner}, баланс: {Balance}.");
+            }
+            else if (amount > Balance)
+            {
+                Console.WriteLine("Недостаточно средств!");
+                Console.WriteLine($"{Owner}, баланс: {Balance}.");
+            }
+            else
+            {
+                Console.WriteLine("Сумма должна быть больше 0!");
+            }
+        }
+    }
+}
