6d0d13251aad64220fb1c12b1d28293a11ed9d05
HoshinoRe <ggamsenitro@gmail.com>
2025-10-21 23:19:49 +0300

    Add files via upload

---
+++ OOP/Number 2/Nu2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            phone myPhone = new phone("Samsung", 50);
+            myPhone.UsePhone();
+            myPhone.UsePhone();
+            myPhone.UsePhone();
+        }
+    }
+}
---
+++ OOP/Number 2/phone.cs
@@ -0,0 +1,45 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class phone
+    {
+        private string Brand;
+        private int Battery;
+
+        public phone(string pBrand, int pBattery)
+        {
+            Brand = pBrand;
+            if (pBattery >= 0 && pBattery <= 100)
+            {
+                Battery = pBattery;
+            }
+            else
+            {
+                Battery = 0;
+                Console.WriteLine("заряд телефона может быть от 1 до 100");
+            }
+        }
+        public void UsePhone()
+        {
+            if (Battery > 0)
+            {
+                Battery -= 10;
+                if (Battery < 0)
+                    Battery = 0;
+                Console.WriteLine($"Телефон {Brand}, заряд: {Battery}%.");
+            }
+            else
+            {
+                Console.WriteLine($"Телефон {Brand} разряжен!");
+            }
+        }
+
+    }
+
+}
