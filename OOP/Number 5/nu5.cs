b00b429d369b4e2e0dcd99639a171e886a2eab37
HoshinoRe <ggamsenitro@gmail.com>
2025-10-22 00:52:40 +0300

    Add files via upload

---
+++ OOP/Number 5/nu5.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp24;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Pet praim = new Pet("Super krutoi pes 999 optimus praim comander destroer apporation aghanim protector spirit", 150);
+                praim.Play();
+                praim.Rest();
+                praim.Play();
+            }
+        }
+    }
+}
